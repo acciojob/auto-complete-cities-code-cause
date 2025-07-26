fa39ef65704dff804dcb473a070488823d7272a7
Ashish <writeashish22@gmail.com>
2025-07-26 16:23:57 +0530

    solved

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
+import Cities from "./Cities";
+
+
+const citySuggestions = [
+  'Delhi',
+  'Mumbai',
+  'Bangalore',
+  'Chennai',
+  'Hyderabad',
+  'Ahmedabad',
+  'Kolkata',
+  'Pune',
+  'Jaipur',
+  'Lucknow'
+];
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <Cities suggestion={citySuggestions}/>
+        
     </div>
   )
 }
---
+++ src/components/Cities.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+
+
+
+const Cities=({suggestion})=>{
+
+    const [input, setInput] = useState('');
+    const [filtered, setFiltered]= useState([]);
+    const [showSuggestion, setShowSuggestion] = useState(false);
+
+
+    function handleChange(e){
+        const value = e.target.value;
+        setInput(value);
+
+       const filteredArray = suggestion.filter(
+  (item) => item.toLowerCase().includes(value.toLowerCase())
+);
+
+
+       setFiltered(filteredArray);
+
+       setShowSuggestion(true);
+
+    }
+
+
+   function handlesave(value){
+        setInput(value)
+
+        setShowSuggestion(false)
+        setFiltered([])
+
+    }
+
+    
+
+   return (
+  <div>
+    <p>Search cities in India</p>
+    <input
+      type="text"
+      placeholder="Type your city name"
+      onChange={handleChange}
+    />
+
+    {showSuggestion && input && (
+      <ul>
+        {filtered.map((value, index) => (
+          <li key={index} onClick={() => handlesave(value)}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+);
+
+}
+
+export default Cities;
